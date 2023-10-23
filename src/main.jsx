b9ded396847f670b6f572f2f07589901202560e4
SKhai77 <orionkhai@gmail.com>
2023-10-23 17:51:03 -0500

    Added the required codes to the file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// Import necessary modules from React and ReactDOM for rendering
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Import routing-related modules from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// Import Bootstrap and custom styles
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+
+// Import React components for the pages
+import App from "./App.jsx";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Resume from "./pages/Resume";
+import Contact from "./pages/Contact";
+
+// Define the accessible routes and components associated with each URL
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "",
+        element: <About />,
+      },
+      {
+        path: "portfolio/:id", // Dynamic route for portfolio with an ID parameter
+        element: <Portfolio />,
+      },
+      {
+        path: "resume",
+        element: <Resume />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+// Render the RouterProvider component, wrapping the entire application in a router
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}>
+    {/* Wrap the entire application in React StrictMode for additional development checks */}
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RouterProvider>
+);
