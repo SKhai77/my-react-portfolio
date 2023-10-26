5561686a4fb8fbb17e33db13d8fac91e5ec12115
SKhai77 <orionkhai@gmail.com>
2023-10-26 18:12:51 -0500

    Update comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-// Import icons
+// Import Fontawesome and the icons
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 
