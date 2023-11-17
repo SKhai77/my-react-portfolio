12c3256c86b05178ebb5ba6f77ecb1ccd949c44c
SKhai77 <orionkhai@gmail.com>
2023-11-17 01:46:31 -0600

    Add project description to display technologies used

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -5,12 +5,14 @@ function Portfolio() {
   const projects = [
     {
       title: "Roam Ready - Travler blog",
+      description: "MERN Stack",
       imageUrl: "./src/assets/images/roam-ready.png",
       githubLink: "https://github.com/SKhai77/roam-ready",
       deployedLink: "https://roam-ready-79c746e8ffc3.herokuapp.com/",
     },
     {
       title: "Books Search Engine",
+      description: "NoSQL/GraphQL/Express/React/Node",
       imageUrl: "./src/assets/images/books-search-engine.png",
       githubLink: "https://github.com/SKhai77/books-search-engine",
       deployedLink:
@@ -18,6 +20,7 @@ function Portfolio() {
     },
     {
       title: "J.A.T.E - Text Editor",
+      description: "IndexedDB/SWA/Webpack",
       imageUrl: "./src/assets/images/jate-text-editor.png",
       githubLink: "https://github.com/SKhai77/jate-text-editor",
       deployedLink:
@@ -25,18 +28,21 @@ function Portfolio() {
     },
     {
       title: "Build Link - Construction job board",
+      description: "MySQL/Express/Nodejs",
       imageUrl: "./src/assets/images/build-link.png",
       githubLink: "https://github.com/SKhai77/build-link",
       deployedLink: "https://build-link-10beabbadf73.herokuapp.com",
     },
     {
       title: "MVC Tech Blog",
+      description: "Bcrypt/Handlebars/Sequelize",
       imageUrl: "./src/assets/images/mvc-tech-blog.png",
       githubLink: "https://github.com/SKhai77/mvc-tech-blog",
       deployedLink: "https://sk77-mvctechblog-e599ac753156.herokuapp.com",
     },
     {
       title: "Work Day Scheduler",
+      description: "HTML/CSS/jQuery",
       imageUrl: "./src/assets/images/work-day-scheduler.png",
       githubLink: "https://github.com/SKhai77/work-day-scheduler",
       deployedLink: "https://skhai77.github.io/work-day-scheduler",
@@ -50,7 +56,10 @@ function Portfolio() {
           <h2>Portfolio</h2>
           <div className="row mt-4">
             {projects.map((project, index) => (
-              <div className="col-sm-6 col-md-4 col-lg-4 col-xxl-3" key={index}>
+              <div
+                className="col-sm-6 col-md-4 col-lg-4 col-xxl-3 g-4"
+                key={index}
+              >
                 <Project
                   title={project.title}
                   description={project.description}
