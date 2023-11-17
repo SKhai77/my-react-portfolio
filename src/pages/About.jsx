0296362ebaa567d97127f6f1b0718e23a90a689b
SKhai77 <orionkhai@gmail.com>
2023-11-17 01:49:04 -0600

    Add more worlds to the file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,12 +16,21 @@ function AboutMe() {
             <p>
               Hello! My name is Sian Khai. I have been passionate about web
               development since 2006, and I learned some basic web design when I
-              was in India. But I wasn't able to continue in the field due to my
-              situation. I have been working as a ride-share driver since May
+              was in India. Unfortunately, I wasn't able to continue in the field due to my
+              situation. 
+            </p>
+            <p>
+              I have been working as a ride-share driver since May of
               2022. Before that, I worked for a non-profit organization for over
               5 years as an employment specialist for newly arrived immigrants
-              and a financial coach.
+              and later as a financial coach. 
             </p>
+            <p>
+              I have completed the Full Stack Web Development Boot Camp at SMU and graduated as of
+              November of 2023. I have been able to create many projects from the Boot Camp
+              using various coding languages, mainly MERN stack. My goal is to acquire a stable
+              career in this field of work.
+            </p> 
           </div>
         </div>
       </div>
