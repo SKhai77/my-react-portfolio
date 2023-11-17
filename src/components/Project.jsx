bdc30c81f3edb0173eb0bb3299b8d4996759c768
SKhai77 <orionkhai@gmail.com>
2023-11-17 01:45:00 -0600

    Change the color and add p element

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -34,7 +34,7 @@ const Project = ({
             width: "100%",
             height: "200px",
             objectFit: "fill",
-            border: isHovered ? "2px solid #917601" : "white",
+            border: isHovered ? "2px solid #dbbe17" : "white",
             opacity: isHovered ? "0.1" : "1",
             transition: "opacity 0.3s ease",
           }}
@@ -51,47 +51,49 @@ const Project = ({
             }}
           >
             <h3>{title}</h3>
+            <p style={{ color: "#dbbe17", fontWeight: "bold" }}>{description}</p>
             <div
               style={{
                 display: "flex",
-                justifyContent: "space-between",
+                justifyContent: "center",
                 marginTop: "20px",
                 opacity: "1",
               }}
             >
-              <a
-                href={githubLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  fontSize: "1rem",
-                  marginRight: "5px",
-                  opacity: "1",
-                  textDecoration: "none",
-                  color: isHovered ? "#917601" : "white",
-                }}
-              >
-                <FontAwesomeIcon icon={faGithub} /> Code
-              </a>
-              <a
-                href={deployedLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  fontSize: "1rem",
-                  marginLeft: "5px",
-                  opacity: "1",
-                  textDecoration: "none",
-                  color: isHovered ? "#917601" : "white",
-                }}
-              >
-                <i className="fa fa-external-link-square"></i> www
-              </a>
+              <p>
+                <a
+                  href={githubLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    fontSize: "1rem",
+                    marginRight: "5px",
+                    opacity: "1",
+                    textDecoration: "none",
+                    color: isHovered ? "#dbbe17" : "white",
+                  }}
+                >
+                  <FontAwesomeIcon icon={faGithub} /> Code
+                </a>
+                <a
+                  href={deployedLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{
+                    fontSize: "1rem",
+                    marginLeft: "5px",
+                    opacity: "1",
+                    textDecoration: "none",
+                    color: isHovered ? "#dbbe17" : "white",
+                  }}
+                >
+                  <i className="fa fa-external-link-square"></i> www
+                </a>
+              </p>
             </div>
           </div>
         )}
       </div>
-      <p>{description}</p>
     </div>
   );
 };
