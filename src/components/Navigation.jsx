addaf8ba9f1fc7d3f19ea0aac194149f7fd0ce28
SKhai77 <orionkhai@gmail.com>
2024-01-07 14:40:25 -0600

    Changed and update the whole navbar and its styles

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,54 +1,70 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-const Navigation = () => {
+function Navigation() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid d-flex justify-content-between align-items-center">
-        <div className="navbar-logo">
-          <NavLink to="/" className="navbar-brand">
-            SIAN | PORTFOLIO
-          </NavLink>
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container className="mt-2">
+        <div className="container-fluid d-flex justify-content-between align-items-center">
+          <Navbar.Brand as={Link} to="/">
+            <h1>SIAN | PORTFOLIO</h1>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
         </div>
-        <div className="navbar-menu">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <NavLink to="/about" className="nav-link active" end>
-                  About Me
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/portfolio" className="nav-link active">
-                  Portfolio
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/resume" className="nav-link active">
-                  Resume
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink to="/contact" className="nav-link active">
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </nav>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto px-4 py-2">
+            <Nav.Link
+              as={NavLink}
+              to="/about"
+              className="ms-md-lg-auto"
+              activeClassName="active"
+              style={{
+                letterSpacing: "3px",
+                marginRight: "20px",
+                marginBottom: "0px"
+              }}
+            >
+              About
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/portfolio"
+              activeClassName="active"
+              style={{
+                letterSpacing: "3px",
+                marginRight: "20px",
+                marginBottom: "0px"
+              }}
+            >
+              Portfolio
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/resume"
+              activeClassName="active"
+              style={{
+                letterSpacing: "3px",
+                marginRight: "20px",
+                marginBottom: "0px"
+              }}
+            >
+              Resume
+            </Nav.Link>
+            <Nav.Link
+              as={NavLink}
+              to="/contact"
+              activeClassName="active"
+              style={{ letterSpacing: "3px", marginRight: "20px", marginBottom: "0px" }}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
 export default Navigation;
