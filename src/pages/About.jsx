46c5fd16237d78248506ca8aed995524949d5d1c
SKhai77 <orionkhai@gmail.com>
2024-01-07 14:51:39 -0600

    Update container class and move the image to the center

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,21 +2,23 @@ function AboutMe() {
   return (
     <main>
       <section>
-        <div className="container-fluid">
+        <div className="container">
           <h2>About me</h2>
           <div className="row mt-4">
             <div className="col-md-12">
-              <img
-                src="./assets/images/my-passport.png"
-                alt="My passport size photo"
-                className="img-fluid rounded-circle pt-3 pb-3"
-                style={{ maxWidth: "150px", maxHeight: "150px" }}
-              />
+              <div className="d-flex justify-content-center align-items-center">
+                <img
+                  src="./assets/images/my-passport.png"
+                  alt="My passport size photo"
+                  className="img-fluid rounded-circle pt-3 pb-3"
+                  style={{ maxWidth: "150px", maxHeight: "150px" }}
+                />
+              </div>
               <p>
-                Hello! My name is Sian Khai. I have been passionate about web
+                Hello, my name is Sian Khai. I have been passionate about web
                 development since 2006, and I learned some basic web design when
-                I was in India. Unfortunately, I wasn't able to continue in the
-                field due to my situation.
+                I was in India. I also took a multimedia developer course at Richland Community College in Dallas. 
+                Unfortunately, I wasn't able to continue in the field due to my situation.
               </p>
               <p>
                 I have been working as a ride-share driver since May of 2022.
