c231af9bcd99101420f6dea355ccaf419260a457
SKhai77 <orionkhai@gmail.com>
2024-10-07 14:52:17 -0500

    Update AI Certificate section

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -20,21 +20,21 @@ function Resume() {
               </a>{" "}
               |
               <a
-                href="https://courses.edx.org/certificates/ecacdb998fe1464eae84908c24904361"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fa-solid fa-link" style={{ padding: "5px" }}></i>
-                AI Applications and Prompt Engineering Certificate
-              </a>
-              |
-              <a
                 href="https://www.credly.com/badges/481c9f18-9732-478e-98bf-423d1d4e94b7/public_url"
                 target="_blank"
                 rel="noopener noreferrer"
               >
                 <i className="fa-solid fa-link" style={{ padding: "5px" }}></i>
                 Full Stack Certificate
+              </a>
+              |
+              <a
+                href="https://courses.edx.org/certificates/ecacdb998fe1464eae84908c24904361"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fa-solid fa-link" style={{ padding: "5px" }}></i>
+                AI Applications and Prompt Engineering Certificate
               </a>
             </p>
             <Row>
@@ -61,6 +61,17 @@ function Resume() {
                   <li>MongoDB, Mongoose</li>
                   <li>REST</li>
                   <li>GraphQL</li>
+                </ul>
+              </Col>
+
+              <Col>
+                <h3 className="mt-3">AI Applications and Prompt Engineering</h3>
+                <ul className="list-group">
+                  <li>Artificial Intelligence</li>
+                  <i>Prompt Engineering</i>
+                  <li>OpenAI API</li>
+                  <li>LangChain</li>
+                  <li>ChatGPT</li>
                 </ul>
               </Col>
             </Row>
