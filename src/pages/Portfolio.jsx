dd38c65bca113bf4fa6ad8e91ce495aada94b5f9
SKhai77 <orionkhai@gmail.com>
2024-10-07 14:05:59 -0500

    Add AI Translator app and update images file type to jpg

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -4,16 +4,23 @@ import Project from "../components/Project";
 function Portfolio() {
   const projects = [
     {
+      title: "AI Multi-Language Translator",
+      description: "OpenAI API/LangChain/Express/Node",
+      imageUrl: "./assets/images/ai-multi-language-translator.jpg",
+      githubLink: "https://github.com/SKhai77/ai-multi-language-translator",
+      deployedLink: "https://ai-multi-language-translator.vercel.app",
+    },
+    {
       title: "Roam Ready - Travler blog",
       description: "MERN Stack",
-      imageUrl: "./assets/images/roam-ready.png",
+      imageUrl: "./assets/images/roam-ready.jpg",
       githubLink: "https://github.com/SKhai77/roam-ready",
       deployedLink: "https://roam-ready-79c746e8ffc3.herokuapp.com/",
     },
     {
       title: "Books Search Engine",
       description: "NoSQL/GraphQL/Express/React/Node",
-      imageUrl: "./assets/images/books-search-engine.png",
+      imageUrl: "./assets/images/books-search-engine.jpg",
       githubLink: "https://github.com/SKhai77/books-search-engine",
       deployedLink:
         "https://skhai77-books-search-engine-20032e3d896f.herokuapp.com",
@@ -21,7 +28,7 @@ function Portfolio() {
     {
       title: "J.A.T.E - Text Editor",
       description: "IndexedDB/SWA/Webpack",
-      imageUrl: "./assets/images/jate-text-editor.png",
+      imageUrl: "./assets/images/jate-text-editor.jpg",
       githubLink: "https://github.com/SKhai77/jate-text-editor",
       deployedLink:
         "https://skhai77-jate-text-editor-7aaafa1ed889.herokuapp.com",
@@ -29,23 +36,16 @@ function Portfolio() {
     {
       title: "Build Link - Construction job board",
       description: "MySQL/Express/Nodejs",
-      imageUrl: "./assets/images/build-link.png",
+      imageUrl: "./assets/images/build-link.jpg",
       githubLink: "https://github.com/SKhai77/build-link",
       deployedLink: "https://build-link-10beabbadf73.herokuapp.com",
     },
     {
       title: "MVC Tech Blog",
       description: "Bcrypt/Handlebars/Sequelize",
-      imageUrl: "./assets/images/mvc-tech-blog.png",
+      imageUrl: "./assets/images/mvc-tech-blog.jpg",
       githubLink: "https://github.com/SKhai77/mvc-tech-blog",
       deployedLink: "https://sk77-mvctechblog-e599ac753156.herokuapp.com",
-    },
-    {
-      title: "Work Day Scheduler",
-      description: "HTML/CSS/jQuery",
-      imageUrl: "./assets/images/work-day-scheduler.png",
-      githubLink: "https://github.com/SKhai77/work-day-scheduler",
-      deployedLink: "https://skhai77.github.io/work-day-scheduler",
     },
   ];
 
@@ -53,13 +53,10 @@ function Portfolio() {
     <main>
       <section>
         <div className="container">
-          <h2 >Portfolio</h2>
+          <h2>Portfolio</h2>
           <div className="row">
             {projects.map((project, index) => (
-              <div
-                className="col-sm-6 col-md-4 g-4"
-                key={index}
-              >
+              <div className="col-sm-6 col-md-4 g-4" key={index}>
                 <Project
                   title={project.title}
                   description={project.description}
